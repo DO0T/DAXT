dab4c817d3038a0aa06367f5e033f76d57e92360
thomasfinnegan14 <finneganthomas5@gmail.com>
2024-04-03 17:07:43 -0400

    Add dynamic dialogue script

---
+++ DAXT/Assets/Scripts/dialogueText.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class dialogueText : MonoBehaviour
+{
+    public TextMeshProUGUI dialogueText;
+
+    public void UpdateDialogue()
+    {
+        
+    }
+}
