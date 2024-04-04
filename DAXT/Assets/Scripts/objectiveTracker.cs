26da970bcc119fc53f7216c2f0aaf2a4bfa65714
thomasfinnegan14 <finneganthomas5@gmail.com>
2024-04-03 22:05:15 -0400

    Strikethrough completed mail task

--- DAXT/Assets/Scripts/objectiveTracker.cs
+++ DAXT/Assets/Scripts/objectiveTracker.cs
@@ -8,11 +8,26 @@ public class objectiveTracker : MonoBehaviour
     public TextMeshProUGUI objectiveList;
     public string mailObjective = "Letter Objective";
 
+    // States
+    public bool mailAdded = false;
+
     void Start() {
         objectiveList.text = "Current Objectives: ";
     }
 
     public void AddMailToTracker() {
         objectiveList.text += "\n" + mailObjective;
+        mailAdded = true;
     }
+
+    // Call this method to update the last added objective text with a strikethrough
+    public void StrikeThroughMailObjective()
+    {
+            // Remove the last added objective text
+            objectiveList.text = objectiveList.text.Replace("\n" + mailObjective, "");
+            
+            // Add it back with strikethrough tags
+            objectiveList.text += "\n<s>" + mailObjective + "</s>";
+    }
+
 }
