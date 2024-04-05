95bbf13031a9352adfb1bb64cdeaf94f8770407d
DO0T <dawsontomes@gmail.com>
2024-04-05 15:48:55 -0400

    finished last minute changes

--- DAXT/Assets/Scripts/triggerNewEntry.cs
+++ DAXT/Assets/Scripts/triggerNewEntry.cs
@@ -7,28 +7,19 @@ public class triggerNewEntry : MonoBehaviour
     public RaycastInteraction triggered;
     public GameObject thisObject;
     public bool trigger;
-    // Start is called before the first frame update
+
     void Start()
     {
         trigger = false;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        trigger = false;
-        if (triggered.hitObject == thisObject) {
-            trigger = true;
-            Debug.Log("Triggered by object");
-            Debug.Log(thisObject);
-        }
-        /*
-        else if ()
+        if (triggered.LastHitObject == thisObject)
         {
             trigger = true;
-            Debug.Log("Triggered by dialog");
-            Debug.Log(thisObject);
+            Debug.Log("Triggered by object: " + thisObject.name);
         }
-        */
+        // You can add an else block or other conditions as needed.
     }
 }
