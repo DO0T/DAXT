cca1d4ac39ffb678f89ab21903570b6688387abc
abcarso <abcarso@clemson.edu>
2024-04-05 12:50:48 -0400

    Update Journal Text

---
+++ DAXT/Assets/Scripts/triggerNewEntry.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class triggerNewEntry : MonoBehaviour
+{
+    public RaycastInteraction triggered;
+    public GameObject thisObject;
+    public bool trigger;
+    // Start is called before the first frame update
+    void Start()
+    {
+        trigger = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        trigger = false;
+        if (triggered.hitObject == thisObject) {
+            trigger = true;
+            Debug.Log("Triggered");
+            Debug.Log(thisObject);
+        }
+    }
+}
