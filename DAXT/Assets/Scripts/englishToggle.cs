e40fe51ec28dd2e359dc8016bab0db6928a914e7
abcarso <abcarso@clemson.edu>
2024-04-17 01:52:22 -0400

    Fix Tutorial Text

--- DAXT/Assets/Scripts/englishToggle.cs
+++ DAXT/Assets/Scripts/englishToggle.cs
@@ -7,12 +7,15 @@ public class englishToggle : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+         if(Input.GetKeyDown(KeyCode.E))
+       {
+            
+       }
     }
 }
